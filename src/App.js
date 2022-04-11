d99c5b7a09d268a275ce05eff31a7848bf9602bf
ilrosaunsam <ilrosa@estudiantes.unsam.edu.ar>
2022-04-11 02:04:04 -0300

    mi primera entrega

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido a <code>Mi Futura Tienda</code> online!
         </p>
         <a
           className="App-link"
